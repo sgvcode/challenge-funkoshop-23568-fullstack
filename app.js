a9eed3a5f5ec5b3d37b71226efa45409d03ea487
SGVcode <sgvero.box@gmail.com>
2023-08-19 13:26:42 -0300

    [FEAT] Creado servidor express

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const app = express();
+const PORT = 3002;
 
 app.use(express.static('public_html'))
 
-app.get('/home', (req, res) => {
+// app.get('/home', (req, res) => {
 
-    //toda la lógica (llamada a bd, etc)
+//     //toda la lógica (llamada a bd, etc)
 
-    res.sendFile(__dirname + '/public_html/index.html')
-}
-);
+//     res.sendFile(__dirname + '/public_html/index.html')
+// }
+// );
 
 
 app.get('/shop', (req, res) => res.sendFile(__dirname + '/public_html/pages/shop/shop.html'));
 app.get('/ping', (req, res) => res.send('pong'));
 
-app.listen(4000, () => console.log("Servidor corriendo en http://localhost:4000"));
+app.listen(PORT, () => console.log(`Servidor corriendo en ${PORT}`));
