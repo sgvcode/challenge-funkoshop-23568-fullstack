9f215f886dc7531d17ff15fd5e6f5373479ee0d0
SGVcode <sgvero.box@gmail.com>
2023-08-19 15:23:29 -0300

    [FEAT] Añadida lógica de enlaces en .js

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ const PORT = 3002;
 
 app.use(express.static('public_html'))
 
-// app.get('/home', (req, res) => {
+app.get('/home', (req, res) => {
 
-//     //toda la lógica (llamada a bd, etc)
+    //toda la lógica (llamada a bd, etc)
 
-//     res.sendFile(__dirname + '/public_html/index.html')
-// }
-// );
+    res.sendFile(__dirname + '/public_html/index.html')
+}
+);
 
+app.get('/contact', (req, res) => res.sendFile(__dirname + '/public_html/contact.html'));
 
 app.get('/shop', (req, res) => res.sendFile(__dirname + '/public_html/pages/shop/shop.html'));
-app.get('/ping', (req, res) => res.send('pong'));
+app.get('/cart', (req, res) => res.sendFile(__dirname + '/public_html/pages/shop/cart.html'));
+app.get('/item', (req, res) => res.sendFile(__dirname + '/public_html/pages/shop/item.html'));
+app.get('/login', (req, res) => res.sendFile(__dirname + '/public_html/pages/admin/login.html'));
 
 app.listen(PORT, () => console.log(`Servidor corriendo en ${PORT}`));
