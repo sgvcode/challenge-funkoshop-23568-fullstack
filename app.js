d8002f1aa7ff263fc139f6b9203740deafc994c8
SGVcode <sgvero.box@gmail.com>
2023-08-24 16:21:35 -0300

    [FEAT] Añadida página de listado de productos - admin.html

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 const express = require('express');
 const app = express();
 const PORT = 3002;
+const itemRoutes = require('./src/routes/itemRoutes');
 
-app.use(express.static('public_html'))
+app.use(express.static('public_html'));
 
-app.get('/home', (req, res) => {
+app.use(express.urlencoded());
+app.use(express.json());
 
-    //toda la lógica (llamada a bd, etc)
 
-    res.sendFile(__dirname + '/public_html/index.html')
-}
-);
+// Middleware a nivel de ruta (paso intermedio)
+app.use('/', itemRoutes);
+
+
+app.get('/home', (req, res) => { res.sendFile(__dirname + '/public_html/index.html') });
 
 app.get('/contact', (req, res) => res.sendFile(__dirname + '/public_html/contact.html'));
-
 app.get('/shop', (req, res) => res.sendFile(__dirname + '/public_html/pages/shop/shop.html'));
 app.get('/cart', (req, res) => res.sendFile(__dirname + '/public_html/pages/shop/cart.html'));
-app.get('/item', (req, res) => res.sendFile(__dirname + '/public_html/pages/shop/item.html'));
 app.get('/login', (req, res) => res.sendFile(__dirname + '/public_html/pages/admin/login.html'));
+app.get('/register', (req, res) => res.sendFile(__dirname + '/public_html/pages/admin/register.html'));
+
+// Admin
+app.get('/admin', (req, res) => res.sendFile(__dirname + '/public_html/pages/admin/admin.html'));
 
 app.listen(PORT, () => console.log(`Servidor corriendo en ${PORT}`));
