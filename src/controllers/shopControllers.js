93766392659741ed4651de37bd3990630f9494e5
SGVcode <sgvero.box@gmail.com>
2023-12-10 03:54:33 -0300

    [FEAT] implentacion del carrito parte 1 sin funcionar borrar y cantidades

--- src/controllers/shopControllers.js
+++ src/controllers/shopControllers.js
@@ -24,53 +24,150 @@ const shopControllers = {
     },
 
     itemView: async (req, res) => {
-        const id = req.params.id;
-        const itemResponse = await ItemsService.getItem(id);
-        const allItemsResponse = await ItemsService.getAllItems();
+        try {
+            const id = req.params.id;
+            const itemResponse = await ItemsService.getItem(id);
+            const allItemsResponse = await ItemsService.getAllItems();
 
-        const { data: item } = itemResponse;
-        const { data: allItems } = allItemsResponse;
+            const { data: item } = itemResponse;
+            const { data: allItems } = allItemsResponse;
 
-        // Filtra los items para incluir solo aquellos de la misma licencia que el item individual
-        const relatedItems = allItems.filter(i => i.licence_id === item[0].licence_id && i.product_id !== item[0].product_id);
+            // Verificar si el item está definido
+            if (!item || !item[0]) {
+                console.error(`Error: No se encontró el producto con ID ${id}`);
+                res.status(404).send('Producto no encontrado');
+                return;
+            }
 
-        res.render('./shop/item', {
-            view: {
-                title: "Items | Funkoshop"
-            },
-            item: item[0],
-            enableGlide: true,
-            sliderTitle: 'Productos Relacionados',
-            items: relatedItems
-        });
-    },
+            // Filtra los items para incluir solo aquellos de la misma licencia que el item individual
+            const relatedItems = allItems.filter(i => i.licence_id === item[0].licence_id && i.product_id !== item[0].product_id);
 
-    addToCart: async (req, res) => {
-        const id = req.params.id;
-        const item = await ItemsService.getItem(id);
-        // console.log(item);
-
-        if (item.data.length > 0) {
-            // Agrega el item al carrito directamente en el controlador
-            req.session.cart = req.session.cart || [];
-            req.session.cart.push(item.data[0]);
-
-            res.send(`Item ${item.data[0].product_name} agregado al carrito.`);
-        } else {
-            res.send('No se encontró el item para agregar al carrito.');
+            res.render('./shop/item', {
+                view: {
+                    title: "Items | Funkoshop"
+                },
+                item: item[0],
+                enableGlide: true,
+                sliderTitle: 'Productos Relacionados',
+                items: relatedItems
+            });
+        } catch (error) {
+            console.error('Error en itemView:', error);
+            res.status(500).send('Error interno del servidor');
         }
     },
 
-    cart: (req, res) => {
-        // Renderiza la página cart.ejs con los elementos del carrito almacenados en la sesión
-        const cartItems = req.session.cart || [];
+    cart: async (req, res) => {
+        try {
+            // Lógica para obtener el carrito (puedes almacenarlo en la sesión o en localStorage)
+            const cart = req.session.cart || []; // Utilizando sesión como ejemplo
 
-        res.render('../views/shop/cart', {
-            view: {
-                title: "Carrito | Funkoshop"
-            },
-            cart: cartItems
-        });
+            res.render('../views/shop/cart', {
+                view: {
+                    title: "Carrito de Compras | Funkoshop"
+                },
+                cart,
+            });
+        } catch (error) {
+            console.error('Error en cart:', error);
+            res.status(500).send('Error interno del servidor');
+        }
+    },
+
+    // shopControllers.js
+    addToCart: async (req, res) => {
+        try {
+            const productId = req.params.id;
+            const quantity = req.body.quantity || 1;
+
+            // Lógica para agregar el producto al carrito
+            req.session.cart = req.session.cart || [];
+            const cartItemIndex = req.session.cart.findIndex(item => String(item.product_id) === String(productId));
+
+            console.log('Antes de la lógica de agregado:', req.session.cart);
+
+            if (cartItemIndex !== -1) {
+                // Si el producto ya está en el carrito, actualiza la cantidad y el total
+                req.session.cart[cartItemIndex].quantity += parseInt(quantity);
+                req.session.cart[cartItemIndex].total = req.session.cart[cartItemIndex].price * req.session.cart[cartItemIndex].quantity;
+            } else {
+                // Aquí, ajustamos la estructura del objeto que se agrega al carrito
+                const itemResponse = await ItemsService.getItem(productId);
+
+                if (!itemResponse || !itemResponse.data || itemResponse.data.length === 0) {
+                    console.error(`Error: No se encontró el producto con ID ${productId}`);
+                    res.status(404).send('Producto no encontrado');
+                    return;
+                }
+
+                const item = itemResponse.data[0];
+
+                const newItem = {
+                    product_id: item.product_id,
+                    image_front: item.image_front,
+                    product_name: item.product_name,
+                    licence_name: item.licence_name,
+                    price: item.price,
+                    quantity: parseInt(quantity),
+                    total: item.price * parseInt(quantity)
+                };
+
+                req.session.cart.push(newItem);
+            }
+
+            console.log('Después de la lógica de agregado:', req.session.cart);
+
+            // Redireccionar a la página del carrito después de agregar
+            console.log('Redireccionando...');
+            res.json({ redirectUrl: '/shop/cart' });
+        } catch (error) {
+            console.error('Error en addToCart:', error);
+            res.status(500).send('Error interno del servidor');
+        }
+    },
+
+    getCartCount: (req, res) => {
+        try {
+            const cart = req.session.cart || [];
+            const cartCount = cart.reduce((count, item) => count + item.quantity, 0);
+
+            res.json({ cartCount });
+        } catch (error) {
+            console.error('Error en getCartCount:', error);
+            res.status(500).json({ error: 'Error interno del servidor' });
+        }
+    },
+
+
+    deleteCart: async (req, res) => {
+        try {
+            const productId = req.params.id;
+
+            // Lógica para eliminar el producto del carrito
+            req.session.cart = req.session.cart || [];
+            req.session.cart = req.session.cart.filter(item => item.productId !== productId);
+
+            res.redirect('/shop/cart'); // Redireccionar a la página del carrito después de eliminar
+        } catch (error) {
+            console.error('Error en deleteCart:', error);
+            res.status(500).send('Error interno del servidor');
+        }
+    },
+
+    updateQuantity: (req, res) => {
+        try {
+            const productId = req.params.productId;
+            const action = req.params.action;
+
+            // Aquí debes implementar la lógica para actualizar la cantidad en el carrito
+            // Puedes acceder al carrito a través de req.session.cart o donde lo tengas almacenado
+
+            // Después de actualizar la cantidad, puedes enviar una respuesta al cliente
+            res.json({ success: true, message: 'Cantidad actualizada exitosamente' });
+        } catch (error) {
+            console.error('Error en updateQuantity:', error);
+            res.status(500).json({ success: false, message: 'Error interno del servidor' });
+        }
     },
 
     checkout: (req, res) => {
--- src/routes/shopRoutes.js
+++ src/routes/shopRoutes.js
@@ -6,13 +6,9 @@ router.get('/', shopControllers.shopView);
 router.get('/item/:id', shopControllers.itemView);
 router.post('/item/:id/add', shopControllers.addToCart);
 router.get('/cart', shopControllers.cart);
+router.get('/cart/count', shopControllers.getCartCount);
+router.post('/updateQuantity/:productId/:action', shopControllers.updateQuantity);
+
 router.post('/cart', shopControllers.checkout);
 
-
-
 module.exports = router;
-// router.get('/', (req, res) => res.send('Route a página Shop'));
-// router.get('/item/:id', (req, res) => res.send(`Route a un producto por su ID:${req.params.id}`));
-// router.post('/item/:id/add', (req, res) => res.send('Route agregar un producto al carrito'));
-// router.get('/cart', (req, res) => res.send('Route a página de carrito'));
-// router.post('/cart', (req, res) => res.send('Route para ir al checkout'));
