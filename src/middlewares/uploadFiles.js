216f65e6f80050aaba51eb47b2a4aa7a840ecd62
SGVcode <sgvero.box@gmail.com>
2023-12-23 16:21:39 -0300

    Feat: restaurando version anterior por error en deploy

--- src/middlewares/uploadFiles.js
+++ src/middlewares/uploadFiles.js
@@ -1,13 +1,8 @@
 const multer = require('multer');
 const path = require('path');
 
-// const storage = multer.diskStorage({
-//     destination: (req, file, cb) => cb(null, path.resolve(__dirname, '../../public_html/img')),
-//     filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`)
-// });
-
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, path.resolve(__dirname, '../../tmp')),
+    destination: (req, file, cb) => cb(null, path.resolve(__dirname, '../../public_html/img')),
     filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`)
 });
 
